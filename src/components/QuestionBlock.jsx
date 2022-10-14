ab6148afee0aabf98d9f9796e49b8bd13d0a3f22
Paul Thomas <paulthomas2107@gmail.com>
2022-10-14 11:51:46 +0100

    More...

--- src/components/QuestionBlock.jsx
+++ src/components/QuestionBlock.jsx
@@ -1,14 +1,17 @@
 const QuestionBlock = ( {question}) => {
   
     return (
+   
         <button className="question-block">
-            <img/>
+            <img src={question.image} alt={question.alt} />
+        
             <h3>{question.text}</h3>
             <p>
-                <a href={question.url}>{question.credit}</a>
+                <a href={question.image}>{question.credit} </a>
                 <a href="https://unsplash.com">Unsplash</a>
             </p>
         </button>
+        
     )
 }
 
--- src/components/QuestionsBlock.jsx
+++ src/components/QuestionsBlock.jsx
@@ -4,9 +4,7 @@ const QuestionsBlocks = ({quizItem}) => {
     
     return (
         <>
-            <h2 id={quizItem.id} className="question-title">
-                {quizItem.text}
-            </h2>
+            <h2 id={quizItem.id} className="question-title">{quizItem.text}</h2>
             <div className="questions-container">
                 {quizItem.questions.map((question, _index) =>(
                     <QuestionBlock key={_index} question={question} />
