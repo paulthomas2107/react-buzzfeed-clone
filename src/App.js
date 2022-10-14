06d94ce107e9686bf5d7afab47366abd25179882
Paul Thomas <paulthomas2107@gmail.com>
2022-10-14 09:23:48 +0100

    Get data works

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import Title from './components/Title'
+import { useState,useEffect } from 'react';
 
 
 
 const App = () => {
+
+  const [quiz, setQuiz] = useState(false)
+
+  const fetchData = async () => {
+
+    try {
+
+      const response = await fetch("http://localhost:8000/quiz")
+      const json = await response.json()
+      console.log(json)
+      setQuiz(json)
+
+    } catch (err) {
+      console.log("Error on data: ", err)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <div>
       <Title/>
